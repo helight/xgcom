3ddf25a5dc3c27f4afe689a16d23cd9b68a8bf76
helight.xu@gmail.com <helight.xu@gmail.com@f1a37b60-82ea-11de-940a-9722bdde947d>
2011-12-16 12:50:24 +0000

    change all words for xgentext

--- trunk/src/MenuCall.c
+++ trunk/src/MenuCall.c
@@ -105,23 +105,23 @@ on_help_xgcom_activate (GtkMenuItem *menuitem, gpointer user_data)
 	gchar help_msg[1024];
 	
 	snprintf(help_msg, 1024, 
-		"<b>软件介绍</b>\n"
-		"一个Linux下的图形化的串口调试工具.帮助开发者调试串口程序. \n\n"
-		"<b>软件功能</b>\n"
-		"1.串口基本收发\n"
-		"2.串口配置(串口,速率,数据位,停止位,奇偶校验,数据控制)\n"
-		"3.串口配置的保存和载入(串口,速率,数据位,停止位,奇偶校验,数据控制)\n"
-		"4.串口HEX显示接收数据\n"
-		"5.串口接收数据和发送数据的计数显示(按字节)\n"
-		"6.串口接收数据的捕获(捕获到固定文件,但可以是追加捕获或是覆盖捕获)\n"
-		"7.文件发送(选择指定文件去发送)\n"
-		"8.串口HEX发送(以16进制的方式发送输入字符)\n"
-		"9.连续定时间间隔发送数据,发送间隔时间可以自己设置\n"
-		"10.面板托盘显示(可以隐藏或显示)\n"
-		"11.终端控制功能(像minicom那样可以直接在终端输入进行响应)\n"
-		"12.可配置默认启动参数.配置后以后启动则以默认启动参数打开串口\n"
-		"13.Ctrl + Enter 快捷键发送数据\n"
-		"14.本地回显.\n");
+		"<b>Intro:</b>\n"
+		"gui tool to help guys debug programs remotely by series port, like minicom.\n\n"
+		"<b>Function</b>\n"
+		"1.Send and Receive data by UART\n"
+		"2.Setting up the UART(speed,data,stop,odd,data control)\n"
+		"3.Save or Load the UART Setting to or from file\n"
+		"4.Show data by HEX\n"
+		"5.Count the date, receive or send(by bety)\n"
+		"6.Save recevie date to file(append or recover)\n"
+		"7.Send file by UART\n"
+		"8.Send by HEX\n"
+		"9.Send by interval, the time can be set\n"
+		"10.Show or hide by tray\n"
+		"11.Control like terminal\n"
+		"12.Conf defalut configure file, xgcom start up by the defaule configure file\n"
+		"13.Send data by Ctrl + Enter\n"
+		"14.Echo show\n");
 
 	create_xgcom_msg ((GtkWidget *)user_data, help_msg);
 }
